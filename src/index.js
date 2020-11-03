2204ec8a83de7d5612e1f51b08890513c52941d6
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-11-02 22:28:52 -0500

    refactor(src/index): add request ApolloClient

--- src/index.js
+++ src/index.js
@@ -4,15 +4,34 @@ import reactDOM from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 
+import Context from './Context'
+
 import {App} from './App'
 
 const client = new ApolloClient({
-  uri: 'https://petgram-server-sg.vercel.app/graphql'
+  uri: 'https://petgram-server-sg.vercel.app/graphql',
+  request: operation => {
+    const token = window.sessionStorage.getItem('token')
+    const authorization = token ? `Bearer ${token}` : ''
+    operation.setContext({
+      headers: {
+        authorization
+      }
+    })
+  },
+  onError: ({ networkError }) => {
+    if (networkError.result.code === 'invalid_token') {
+      window.localStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
 })
 
 reactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Context.Provider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Context.Provider>,
   document.getElementById('app')
 )
