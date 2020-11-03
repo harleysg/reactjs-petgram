c6b58f58134ac81d1a22bf4be2054978aa21db92
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-11-02 22:29:26 -0500

    refactor(App.js): add Helmet component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
 import { Routes } from './pages'
-import { Logo } from './components'
+import { Logo, NavBar } from './components'
 
 import { GlobalStyles } from "./styles";
 
+import { Helmet } from 'react-helmet'
+
 export function  App () {
   return <>
+    <Helmet>
+        <title>Petgram - Fotos de animales</title>
+        <meta name='description' content='Tu aplicación para descubrir fotos de animales domésticos, hecha con React ⚛️' />
+    </Helmet>
     <GlobalStyles/>
     <Logo />
     <Routes />
+    <NavBar />
   </>
 }
