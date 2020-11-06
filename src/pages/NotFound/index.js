14da53d1556db1c4a85b49feb5b76b86c8f06544
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-11-06 16:19:43 -0500

    feat(components/NotFound): New component

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Layout } from '../../components'
+
+export function NotFound () {
+  return <Layout
+    title="Page not found 😓"
+    subtitle='La página que buscas no existe'></Layout>
+}
