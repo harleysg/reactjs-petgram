594bb9f342ba4af6a240ae2be62ff1b6f85e2471
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-11-06 13:11:26 -0500

    refactor(pages/User): ajuste estilo componente Grid

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,11 +2,12 @@ import React, { useContext } from 'react'
 
 import { Context } from '../../Context'
 import { Button } from "../../components";
+import { Grid } from './styles'
 
 export function User () {
   const { removeAuth } = useContext(Context)
-  return <>
+  return <Grid>
     <h3>Profile</h3>
     <Button onClick={removeAuth}>Logout</Button>
-  </>
+  </Grid>
 }
---
+++ src/pages/User/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const Grid = styled.div`
+padding-top: 2em;
+`
