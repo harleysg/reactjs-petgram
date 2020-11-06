ec778aac964ba93c3cd01be999f4b96eb496790c
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-11-06 11:12:13 -0500

    refactor(Logo): se cambia componente login por version 2 de svg

---
+++ src/components/Logo/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import { Link as Anchor } from '@reach/router'
+
+export const Svg = styled.svg`
+  max-width: 200px
+`
+export const Link = styled(Anchor)`
+  display: grid
+`
