ca3422af971a7e319f0f7365565ecfc861b2be76
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-11-06 16:21:55 -0500

    refactor(Logo/style): cambio tamaño logo

--- src/components/Logo/style.js
+++ src/components/Logo/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { Link as Anchor } from '@reach/router'
 
 export const Svg = styled.svg`
-  max-width: 200px
+  max-width: 150px
 `
 export const Link = styled(Anchor)`
   display: grid
