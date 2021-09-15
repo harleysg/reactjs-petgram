a5a72414f8898c6575b29893a94e45e224a3eda2
harley santos garzon <harley.santos.garzon@gmail.com>
2021-09-15 00:13:17 -0500

    refactor * improve global styles

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
+import { reset } from './styles/reset'
 
-import {reset} from './styles/reset'
+import pattern from './assets/images/pattern.svg'
 
 export const GlobalStyles = createGlobalStyle`
 
@@ -16,13 +17,32 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     background: #fefefe;
-    max-width: 500px;
+    scroll-behavior: smooth;
   }
-  
+
+  body::before {
+    content: '';
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    opacity: 0.8;
+    background-image: url(${pattern});
+    background-attachment: fixed;
+    background-position: center;
+    background-size: 727px 1080px;
+}
+
   #app {
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, .05);
-    overflow-x: hidden;
+    overflow: hidden;
     min-height: 100vh;
+    max-width: 500px;
+    margin: auto;
     padding-bottom: 10px;
+    background-color: #ffffffc2;
+    backdrop-filter: blur(5px);
+    box-shadow: 0px 0px 10px rgb(0 0 0 / 5%), 0 0 440px 340px white;
   }
 `
